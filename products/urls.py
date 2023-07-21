c01af420e63477994c6b75b89c79c3f01d7a127e
Abhi6722 <abhinav6722cool@gmail.com>
2023-07-21 23:29:24 +0200

    queryset API Methods

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import ProductList,ProductDetail , BrandDetail, BrandList
+from .views import ProductList,ProductDetail , BrandDetail, BrandList , post_list_debug
 
 app_name = 'Products'
 urlpatterns = [
+    path('debug',post_list_debug),
     path('',ProductList.as_view(),name='product_list'),
     path('<slug:slug>',ProductDetail.as_view(),name='product_detail'),
-
-
     path('brand/',BrandList.as_view(),name='brand_list'),
     path('brand/<slug:slug>',BrandDetail.as_view(),name='brand_detail'),
 ]
