dcf6a1d8884aa7a451672f7e45d390c6f8064013
Abhi6722 <abhinav6722cool@gmail.com>
2023-07-21 23:56:23 +0200

    add product_count in home , brands section

--- settings/views.py
+++ settings/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from Products.models import Product , Brand , Review
-
+from django.db.models.aggregates import Count
 
 
 # Create your views here.
 def home(request):
-    brands = Brand.objects.all()
+    brands = Brand.objects.all().annotate(posts_count=Count('product_brand'))
     sale_products = Product.objects.filter(flag='Sale')[0:10]
     feature_products = Product.objects.filter(flag='Feature')[0:6]
     new_products = Product.objects.filter(flag='New')[0:6]
